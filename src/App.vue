51d1039f3e3a0b0102894c37903d769df8c19e87
paul <gspaurgen@gmail.com>
2025-05-25 22:43:05 +0530

    theme config improvement

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Chart from './components/Chart.vue'
 
 <template>
   <div class="bg-primary-800 w-[500px] h-[100px]">
-    <!-- <Chart /> -->
   </div>
   <div class="bg-red-500 w-[500px] h-[100px]"></div>
+  <div class="bg-og-gray w-[500px] h-[100px]"></div>
 </template>
